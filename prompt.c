10fc84f0cbd659868943fc00a39f6578ab503b33
salagreg <greg.sala@icloud.com>
2025-08-19 00:41:59 +0200

    MAJ fichiers

--- prompt.c
+++ prompt.c
@@ -4,8 +4,6 @@
  * @void: Aucun parametre
  * Return: Toujours 0 (Succes), ou 1 si une erreur se produit
  */
-extern char **environ;
-
 int main(void)
 {
 	char *lineptr = NULL;
@@ -23,23 +21,22 @@ int main(void)
 			free(lineptr);
 			exit(0);
 		}
-		if (lineptr[nread - 1] == '\n') {
+		if (lineptr[nread - 1] == '\n')
 			lineptr[nread - 1] = '\0';
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			char *argv[2];
-			argv[0] = lineptr;
-			argv[1] = NULL;
-			execve(argv[0], argv, environ);
-			perror(lineptr);
-			exit(1);
-		}
-		else if (pid > 0)
+
+			pid = fork();
+			if (pid == 0)
+			{
+				char *argv_exec[2];
+				argv_exec[0] = lineptr;
+				argv_exec[1] = NULL;
+				execve(lineptr, argv_exec, environ);
+				perror(lineptr);
+				exit(1);
+			}
+			else if (pid > 0)
 			wait(NULL);
-	
-	}
-	free(lineptr);
-	return (0);
+			}
+			free(lineptr);
+			return (0);
 }
