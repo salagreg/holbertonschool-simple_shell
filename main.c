c63de2560be03f9fe5bf8a827ffe35633b561b0e
root <dorian.oufer@gmail.com>
2025-08-19 09:57:13 +0000

    supp affichage prompt non interactif

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * main - Point d'entree du programme
+ * @void: Aucun parametre
+ * Return: Toujours 0 (Succes), ou 1 si une erreur se produit
+ */
+
+extern char **environ;
+
+int main(void)
+{
+    char *lineptr = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    while (1)
+    {
+	if (isatty(STDIN_FILENO))
+		printf("$ ");
+        nread = getline(&lineptr, &len, stdin);
+        if (nread == -1)
+        {
+            free(lineptr);
+            exit(0);
+        }
+        if (lineptr[nread - 1] == '\n')
+            lineptr[nread - 1] = '\0';
+        if (lineptr[0] == '\0')
+            continue;
+        pid = fork();
+        if (pid == 0)
+        {
+            char *argv[2];
+            argv[0] = lineptr;
+            argv[1] = NULL;
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                perror("Error");
+                exit(1);
+            }
+        }
+            else if (pid > 0)
+            {
+            wait(NULL);
+            }
+            else
+            {
+                perror("fork");
+            }
+        }
+        free(lineptr);
+        return (0);
+}
