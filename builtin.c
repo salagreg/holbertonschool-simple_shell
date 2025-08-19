75563c042ca4fea924bd958b1be3336936a6726a
root <dorian.oufer@gmail.com>
2025-08-19 14:41:27 +0000

    ajout fonction env

--- builtin.c
+++ builtin.c
@@ -9,11 +9,20 @@
  */
 
 int handle_builtin(char **argv, char *lineptr)
+int index;
 {
     if (strcmp(argv[0], "exit") == 0)
     {
         free(lineptr);
         exit(0);
+    }
+    if (strcmp(argv[0],"env") == 0)
+    {
+	    for (i = 0; environ[i] !=NULL; index++)
+	    {
+		    printf("%s_n", environ[index]);
+	    }
+	    return(1);
     }
 
     return (0);
