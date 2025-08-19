72db230022e51f5205d6337278c263d172575051
root <dorian.oufer@gmail.com>
2025-08-19 09:27:06 +0000

    correction MAJ fichier

--- prompt.c
+++ prompt.c
@@ -4,42 +4,50 @@
  * @void: Aucun parametre
  * Return: Toujours 0 (Succes), ou 1 si une erreur se produit
  */
+
 extern char **environ;
 
 int main(void)
 {
-	char *lineptr = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	pid_t pid;
-
-	while (1)
-	{
-		printf("$ ");
-		nread = getline(&lineptr, &len, stdin);
-
-		if (nread == -1)
-		{
-			free(lineptr);
-			exit(0);
-		}
-		if (lineptr[nread - 1] == '\n') {
-			lineptr[nread - 1] = '\0';
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			char *argv[2];
-			argv[0] = lineptr;
-			argv[1] = NULL;
-			execve(argv[0], argv, environ);
-			perror(lineptr);
-			exit(1);
-		}
-		else if (pid > 0)
-			wait(NULL);
-	
-	}
-	free(lineptr);
-	return (0);
+    char *lineptr = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    while (1)
+    {
+        printf("$ ");
+        nread = getline(&lineptr, &len, stdin);
+        if (nread == -1)
+        {
+            printf("\nExit shell...\n");
+            free(lineptr);
+            exit(0);
+        }
+        if (lineptr[nread - 1] == '\n')
+            lineptr[nread - 1] = '\0';
+        if (lineptr[0] == '\0')
+            continue;
+        pid = fork();
+        if (pid == 0)
+        {
+            char *argv[2];
+            argv[0] = lineptr;
+            argv[1] = NULL;
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                perror("Error");
+                exit(1);
+            }
+        }
+            else if (pid > 0)
+            {
+            wait(NULL);
+            }
+            else
+            {
+                perror("fork");
+            }
+        }
+        free(lineptr);
+        return (0);
 }
