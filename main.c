71b4fbeed713f017e401442fb86ae498742cd1c4
root <dorian.oufer@gmail.com>
2025-08-20 14:12:14 +0000

    correction echec path

--- main.c
+++ main.c
@@ -1,38 +1,63 @@
 #include "main.h"
+
+extern char **environ;
 /**
  * main - Point d'entree du programme
  * @void: Aucun parametre
  * Return: Toujours 0 (Succes), ou 1 si une erreur se produit
  */
-
 int main(void)
 {
-	char *lineptr = NULL;
-	size_t len = 0;
-	ssize_t nread;
+    char *lineptr = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
 
-	while (1)
-	{
-		char *argv[64];
+    while (1)
+    {
+        char *argv[64];
+        if (isatty(STDIN_FILENO))
+        {
+            printf("$ ");
+            fflush(stdout);
+        }
+        nread = getline(&lineptr, &len, stdin);
+        if (nread == -1)
+        {
+            free(lineptr);
+            exit(0);
+        }
+        if (lineptr[nread - 1] == '\n')
+            lineptr[nread - 1] = '\0';
 
-		if (isatty(STDIN_FILENO))
-			printf("$ ");
-		nread = getline(&lineptr, &len, stdin);
-		if (nread == -1)
-		{
-			free(lineptr);
-			exit(0);
-		}
-		if (lineptr[nread - 1] == '\n')
-			lineptr[nread - 1] = '\0';
-		if (lineptr[0] == '\0')
-			continue;
-
-		parse_command(lineptr, argv);
-
-		if (handle_builtin(argv, lineptr))
+        if (lineptr[0] == '\0')
+            continue;
+            
+        parse_command(lineptr, argv);
+        
+        if (handle_builtin(argv, lineptr))
 		continue;
-	}
-	free(lineptr);
-	return (0);
+
+        pid = fork();
+        if (pid == 0)
+        {
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                perror("Error");
+                exit(1);
+            }
+        }
+            else if (pid > 0)
+            {
+            wait(NULL);
+            }
+            
+            else
+            {
+                perror("fork");
+            }
+        
+        free(lineptr);
+        return (0);
+    }
 }
